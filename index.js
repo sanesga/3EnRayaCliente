9956a399c0a0b20974404bb1acd31c9ad6dce3d3
sandra <sanesga@gmail.com>
2019-11-03 00:30:18 +0100

    Añadida : Base

---
+++ index.js
@@ -0,0 +1,13 @@
+var usuario1 = new Array();
+usuario1.push(false);
+
+
+
+
+function marcar(id){
+//console.log(posicion);
+var posicion=id.split("-");
+console.log(posicion); //nos devuelve un array de 2 posiciones
+
+
+}
